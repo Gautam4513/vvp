ec919719e416aa57ad3a1b181613a2b8dc44f522
Gautam4513 <gautammandaviya21@gmail.com>
2023-09-15 20:05:46 +0530

    Update p5.c

--- DS experiment/p2/p5.c
+++ DS experiment/p2/p5.c
@@ -4,12 +4,31 @@ specific position.*/
 int main()
 {
     int a[20];
-    int n,i,pos;
+    int n,i,pos,j;
     printf("enter a size of array : ");
     scanf("%d",&n);
     for(i=0;i<n;i++)
     {
-        
+        printf("enter a a[%d]=",i+1);
+        scanf("%d",&a[i]);   
+    }
+    printf("you enter this:");
+    for(i=0;i<n;i++)
+    {
+        printf("\na[%d]=%d",i+1,a[i]);   
+    }
+    printf("\nenter a position where you add a value : ");
+    scanf("%d",&pos);
+    for(i=n;i>=pos-1;i--)
+    {
+        a[i]=a[i-1];
+    }
+    printf("a[%d]=",pos);
+    scanf("%d",&a[pos-1]);
+    i=0;
+    for(i=0;i<n+1;i++)
+    {
+        printf("\na[%d]=%d",i+1,a[i]);   
     }
     return 0;
 }
